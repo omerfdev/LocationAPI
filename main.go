024504eade49449c48e74c7b60e0efac16f9072a
Ömer Almalı <78663695+omerfdev@users.noreply.github.com>
2024-04-20 00:06:25 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,26 +7,19 @@ import (
 	"net/http"
 )
 
-const apiURL = "http://api.ipstack.com/"
+const (
+	apiURL  = "http://api.ipstack.com/"
+	apiKey  = "3771e98c40d2fcb9045fa43e999d8e10"
+	listenAddr = ":8080"
+)
 
 type IPInfo struct {
-	Status      string  `json:"status"`
-	Country     string  `json:"country"`
-	CountryCode string  `json:"countryCode"`
-	Region      string  `json:"region"`
-	RegionName  string  `json:"regionName"`
-	City        string  `json:"city"`
-	Zip         string  `json:"zip"`
-	Lat         float64 `json:"lat"`
-	Lon         float64 `json:"lon"`
-	Timezone    string  `json:"timezone"`
-	Isp         string  `json:"isp"`
-	Org         string  `json:"org"`
-	As          string  `json:"as"`
-	Query       string  `json:"query"`
+	Country     string `json:"country"`
+	City        string `json:"city"`
+	Isp         string `json:"isp"`
 }
 
-func getIPFromRequest(r *http.Request) (string, error) {
+func getIPFromRequest(r *http.Request) string {
 	ip := r.Header.Get("X-Real-IP")
 	if ip == "" {
 		ip = r.Header.Get("X-Forwarded-For")
@@ -34,43 +27,37 @@ func getIPFromRequest(r *http.Request) (string, error) {
 			ip = r.RemoteAddr
 		}
 	}
-	return ip, nil
+	return ip
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ipAddress, err := getIPFromRequest(r)
-		if err != nil {
-			fmt.Println("IP adresi alınamadı:", err)
-			return
-		}
-		apiKey := "3771e98c40d2fcb9045fa43e999d8e10"
-
-		url := fmt.Sprintf("%s%s?access_key=%s", apiURL, ipAddress, apiKey)
+		ip := getIPFromRequest(r)
+		url := fmt.Sprintf("%s%s?access_key=%s", apiURL, ip, apiKey)
 
 		response, err := http.Get(url)
 		if err != nil {
-			fmt.Println("API çağrısı sırasında bir hata oluştu:", err)
+			http.Error(w, "API çağrısı sırasında bir hata oluştu", http.StatusInternalServerError)
 			return
 		}
 		defer response.Body.Close()
 
 		body, err := ioutil.ReadAll(response.Body)
 		if err != nil {
-			fmt.Println("API yanıtını okurken bir hata oluştu:", err)
+			http.Error(w, "API yanıtını okurken bir hata oluştu", http.StatusInternalServerError)
 			return
 		}
 
 		var ipInfo IPInfo
 		err = json.Unmarshal(body, &ipInfo)
 		if err != nil {
-			fmt.Println("JSON çözümleme hatası:", err)
+			http.Error(w, "JSON çözümleme hatası", http.StatusInternalServerError)
 			return
 		}
 
-		fmt.Printf("IP Adresi: %s\nÜlke: %s\nŞehir: %s\nISP: %s\n", ipInfo.Query, ipInfo.Country, ipInfo.City, ipInfo.Isp)
+		fmt.Fprintf(w, "IP Adresi: %s\nÜlke: %s\nŞehir: %s\nISP: %s\n", ip, ipInfo.Country, ipInfo.City, ipInfo.Isp)
 	})
 
-	fmt.Println("Server listening on :8080...")
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Server listening on %s...\n", listenAddr)
+	http.ListenAndServe(listenAddr, nil)
 }
